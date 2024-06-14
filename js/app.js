957117e6d328f798373a1d1f53dff7f9b20138af
Sannusharma02 <Sannusharma02@gmail.com>
2024-06-14 20:48:55 +0530

    giy

--- js/app.js
+++ js/app.js
@@ -8,15 +8,41 @@ $('.slider').slick({
 
 
 let hamberger = document.querySelector('.hamberger');
-let times = document.querySelector('.times');
+let x = document.querySelector('.x');
+let home = document.querySelector('.home');
+let about = document.querySelector('.about');
+let services = document.querySelector('.services');
+let work = document.querySelector('.work');
+let blog = document.querySelector('.blog');
+
 let mobileNav = document.querySelector('.mobile-nav');
 
 hamberger.addEventListener('click', function(){
   mobileNav.classList.add('open');  
 });
 
-times.addEventListener('click', function(){
+x.addEventListener('click', function(){
     mobileNav.classList.remove('open');  
+});
+
+home.addEventListener('click', function(){
+  mobileNav.classList.remove('open');  
+});
+
+about.addEventListener('click', function(){
+  mobileNav.classList.remove('open');  
+});
+
+services.addEventListener('click', function(){
+  mobileNav.classList.remove('open');  
+});
+
+work.addEventListener('click', function(){
+  mobileNav.classList.remove('open');  
+});
+
+blog.addEventListener('click', function(){
+  mobileNav.classList.remove('open');  
 });
 
 });
